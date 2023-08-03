48fee640c1f2fd810195daccd8411547a3b8c6f7
Emil <ekala001@gmail.com>
2023-08-03 16:29:07 +0200

    wip

--- packages/wox-store/src/signal.test.ts
+++ packages/wox-store/src/signal.test.ts
@@ -1,8 +1,44 @@
 import { test, expect } from 'vitest';
 import { signal } from './signal';
 
-test('signal', () => {
-	const value = signal(0);
+test('writable signal, when get method is invoked, should return the value', () => {
+	const value = signal(1);
 
-	expect(value.get());
+	expect(value.get()).toBe(1);
+});
+
+test('writable signal, when set method is invoked, should set the previous value to the new value', () => {
+	const value = signal(1);
+
+	value.set(2);
+
+	expect(value.get()).toBe(2);
+});
+
+test('writable signal, when method is invoked, should compute a new value from the previous value', () => {
+	const value = signal(1);
+
+	value.update((v) => v + 1);
+
+	expect(value.get()).toBe(2);
+});
+
+test('writable signal, when mutate is invoked, should allow mutations to occur on the current state', () => {
+	const value = signal({ x: 1 });
+
+	value.mutate((v) => {
+		v.x = 2;
+	});
+
+	expect(value.get()).toEqual({ x: 2 });
+});
+
+test('writable signal, when mutate is invoked, should dispatch update', () => {
+	const value = signal({ x: 1 });
+
+	value.mutate((v) => {
+		v.x = 2;
+	});
+
+	expect(value.get()).toEqual({ x: 2 });
 });
--- packages/wox-store/src/signal.ts
+++ packages/wox-store/src/signal.ts
@@ -1,13 +1,49 @@
-class Signal {
-	constructor(private value: any) {
+import { EventEmitter, Disposer } from '@wox-team/wox-app-vitals';
+
+export class Signal<T> {
+	private readonly dispatch = new EventEmitter<{
+		update: Readonly<T>;
+	}>();
+
+	constructor(private value: T) {
 		// Empty
 	}
 
-	public get() {
+	public sub(fn: (value: T) => void): Disposer {
+		return this.dispatch.on('update', fn);
+	}
+
+	public get(): T {
 		return this.value;
+	}
+
+	public set(value: T): void {
+		this.value = value;
+
+		this.dispatch.emit('update', this.value);
+	}
+
+	public update(fn: (value: Readonly<T>) => T): void {
+		try {
+			this.set(fn(this.value));
+		} catch (e) {
+			console.error(e);
+		}
+	}
+
+	public mutate(fn: (value: T) => void): void {
+		try {
+			fn(this.value);
+
+			this.dispatch.emit('update', this.value);
+		} catch (e) {
+			console.error(e);
+		}
 	}
 }
 
-export function signal(value: any) {
-	return new Signal(value);
+export function signal<T>(initialValue: T) {
+	const signal = new Signal(initialValue);
+
+	return signal;
 }
