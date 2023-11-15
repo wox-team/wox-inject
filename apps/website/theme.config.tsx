f00690373ea30db53d9af1543b8ccbe8dcec9b2f
Emil <ekala001@gmail.com>
2023-11-15 21:01:51 +0100

    Adds basic docs

---
+++ apps/website/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+	logo: <span>wox</span>,
+	project: {
+		link: 'https://github.com/wox-team/wox-inject',
+	},
+	docsRepositoryBase: 'https://github.com/wox-team/wox-inject',
+	footer: {
+		text: 'Built with Nextra Docs Template',
+	},
+};
+
+export default config;
