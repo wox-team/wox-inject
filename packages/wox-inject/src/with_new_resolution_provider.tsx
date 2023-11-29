72c7431633593eba3091216abff7c184d97e747d
Emil Nordling <ekala001@gmail.com>
2023-11-29 23:12:26 +0100

    #8 Renames withNewResolutionProvider -> withNewContainer

--- packages/wox-inject/src/with_new_resolution_provider.tsx
+++ packages/wox-inject/src/with_new_resolution_provider.tsx
@@ -1,8 +1,13 @@
 import { ResolutionProvider } from './inject_react';
 import { type ComponentType } from 'react';
 
-export function withNewResolutionProvider<T extends Record<string, unknown>>(Component: ComponentType<T>) {
+export function withNewContainer<T extends Record<string, unknown>>(Component: ComponentType<T>) {
 	return function NewDependencyScope(props: T) {
 		return <ResolutionProvider>{<Component {...props} />}</ResolutionProvider>;
 	};
 }
+
+/**
+ * @deprecated use "withNewContainer" instead.
+ */
+export const withNewResolutionProvider = withNewContainer;
